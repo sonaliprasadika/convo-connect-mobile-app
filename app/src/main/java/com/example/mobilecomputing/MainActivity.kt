2de190cef48f4594051131fdd81d1f9f0b99ead1
sonaliprasadika <sonaliprasadika077@gmail.com>
2024-01-18 00:23:37 +0200

    add image

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -1,27 +1,69 @@
 package com.example.mobilecomputing
 
+import android.content.ClipDescription
+import android.icu.text.CaseMap.Title
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mobilecomputing.ui.theme.MobileComputingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(horizontalAlignment = ) {
-                Text("Hello")
-                Text("sonali")
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)){
+       androidx.compose.foundation.Image(
+           painter = painterResource(R.drawable.snow_man),
+           contentDescription = "Contact profile picture",
+           modifier = Modifier
+               .size(40.dp)
+               .clip(CircleShape)
+           )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+
