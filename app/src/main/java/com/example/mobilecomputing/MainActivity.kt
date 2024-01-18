fc2ce34f817dbe78ae8657ac66a27ec2d6ef8d64
sonaliprasadika <sonaliprasadika077@gmail.com>
2024-01-19 00:01:29 +0200

    add clickable event

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mobilecomputing
 
 import android.content.ClipDescription
+import android.content.res.Configuration
 import android.icu.text.CaseMap.Title
 import android.media.Image
 import android.os.Bundle
@@ -9,6 +10,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -27,6 +29,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,6 +40,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mobilecomputing.ui.theme.MobileComputingTheme
+import androidx.compose.runtime.getValue import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,6 +53,12 @@ class MainActivity : ComponentActivity() {
 
 data class Message(val author: String, val body: String)
 
+//@Preview(name = "Light Mode")
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    showBackground = true,
+//    name="Dark Mode"
+//)
 @Composable
 fun MessageCard(msg: Message) {
     Row (modifier = Modifier.padding(all = 8.dp)){
@@ -56,12 +68,36 @@ fun MessageCard(msg: Message) {
            modifier = Modifier
                .size(40.dp)
                .clip(CircleShape)
+               .border(2.dp, MaterialTheme.colorScheme.primary, CircleShape)
            )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = msg.author)
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
