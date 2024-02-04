b4345159e2cb7aab7b40412ab2d9fc79a48f7e42
sonaliprasadika <sonaliprasadika077@gmail.com>
2024-02-04 10:47:04 +0200

    setup room database

--- app/src/main/java/com/example/mobilecomputing/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing/MainActivity.kt
@@ -1,70 +1,82 @@
 package com.example.mobilecomputing
-
-import android.content.ClipDescription
-import android.content.res.Configuration
-import android.icu.text.CaseMap.Title
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.ui.Alignment
-import androidx.navigation.NavHostController
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import com.example.mobilecomputing.data.NotesDatabase
+import com.example.mobilecomputing.presentation.AddNoteScreen
+import com.example.mobilecomputing.presentation.NotesScreen
+import com.example.mobilecomputing.presentation.NotesViewModel
 import com.example.mobilecomputing.ui.theme.MobileComputingTheme
 class MainActivity : ComponentActivity() {
 
-    lateinit var navController: NavHostController
+//    lateinit var navController: NavHostController
+    private val database by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            NotesDatabase::class.java,
+            "notes.db"
+        ).build()
+    }
+
+    private val viewModel by viewModels<NotesViewModel> (
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun<T: ViewModel> create(modelClass: Class<T>): T {
+                    return NotesViewModel(database.dao) as T
+                }
+            }
+        }
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MobileComputingTheme{
-                navController = rememberNavController()
-                SetupNavGraph(navController = navController)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                    val state by viewModel.state.collectAsState()
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "NotesScreen") {
+                        composable("NotesScreen") {
+                            NotesScreen(
+                                state = state,
+                                navController = navController,
+                                onEvent = viewModel::onEvent
+                            )
+                        }
+                        composable("AddNoteScreen") {
+                            AddNoteScreen(
+                                state = state,
+                                navController = navController,
+                                onEvent = viewModel::onEvent
+                            )
+                        }
+                    }
+
+                }
+//                navController = rememberNavController()
+//                SetupNavGraph(navController = navController)
             }
         }
     }
 }
+
 
 
 
---
+++ app/src/main/java/com/example/mobilecomputing/presentation/NotesEvent.kt
@@ -0,0 +1,14 @@
+package com.example.mobilecomputing.presentation
+
+import com.example.mobilecomputing.data.Note
+
+sealed interface NotesEvent {
+    object SortNotes: NotesEvent
+
+    data class DeleteNote(val note: Note): NotesEvent
+
+    data class SaveNote(
+        val title: String,
+        val description: String
+    ): NotesEvent
+}
